0e0f91080c795467c61a901995bd7a0be6e99877
Leticia Souza <leticiasouza.baptista05@gmail.com>
2026-05-14 14:51:50 -0300

    Criada a classe Motorista

---
+++ rota-compartilhada/src/main/java/br/trabalho/Motorista.java
@@ -0,0 +1,15 @@
+package br.trabalho;
+public class Motorista {
+
+    private String nome;
+    private String cpf;
+    private Endereco endereco;
+    private DadosVeiculo dadosVeiculo;
+
+    public Motorista(String nome, String cpf){
+
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+}
