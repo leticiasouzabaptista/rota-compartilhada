f86413351a3e23509f351c681b2c75e1c3bed027
Leticia Souza <leticiasouza.baptista05@gmail.com>
2026-05-25 13:36:01 -0300

    Alterações na classe motorista

--- rota-compartilhada/src/main/java/br/trabalho/Motorista.java
+++ rota-compartilhada/src/main/java/br/trabalho/Motorista.java
@@ -1,4 +1,7 @@
 package br.trabalho;
+
+import java.util.Scanner;
+
 public class Motorista {
 
     private String nome;
@@ -6,26 +9,98 @@ public class Motorista {
     private Endereco endereco;
     private Veiculo veiculo;
 
+    private Scanner scanner;
+
     public Motorista(String nome, String cpf, Endereco endereco, Veiculo veiculo){
 
         this.nome = nome;
         this.cpf = cpf;
         this.endereco = endereco;
         this.veiculo = veiculo;
+
+        scanner = new Scanner(System.in);
     }
 
-    public void setEndereco(String tipoLogadouro, String logadouro, int numero, String bairro, String cidade, String estado, String cep, String pais){
+    public String getCpf(){
+        return cpf;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void exibeDados(){
+        System.out.printf("Nome: %s\n", nome);
+        System.out.printf("Cpf: %s\n", cpf);
+        endereco.exibeEndereco();
+        veiculo.exibeVeiculo();
+    }
+
+    public void setEndereco(){
+       
+        System.out.println("\nEndereco");
+        System.out.print("Tipo do Logadouro: ");
+        String tipoLogadouro = scanner.nextLine();
+        System.out.print("Logadouro: ");
+        String logadouro = scanner.nextLine();
+        System.out.print("Nº: ");
+        int numero = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Bairro: ");
+        String bairro = scanner.nextLine();
+        System.out.print("CEP: ");
+        String cep = scanner.nextLine();
+        System.out.print("Cidade: ");
+        String cidade = scanner.nextLine();
+        System.out.print("Estado: ");
+        String estado = scanner.nextLine();
+        System.out.print("Pais: ");
+        String pais = scanner.nextLine();
+
+        boolean alteracaoValida = false;
+        while(!alteracaoValida){
+            try{
+                endereco.setEndereco(tipoLogadouro, logadouro, numero, bairro, cidade, estado, cep, pais);
+                alteracaoValida = true;
+            }
+            catch(IllegalArgumentException e){
+                System.out.println("Erro ao alterar endereco.");
+                System.out.println(e.getMessage());
+                System.out.println("Digite novamente o tipo de Logadouro: ");
+                tipoLogadouro = scanner.nextLine();
+            }
+        }
+    }
+
+    public void setVeiculo(){
         
-        if(!Endereco.tipoLogadouroEhValido(tipoLogadouro))
-            throw new IllegalArgumentException("Tipo de Logadouro inválido.");
-        endereco.setEndereco(tipoLogadouro, logadouro, numero, bairro, cidade, estado, cep, pais);
-    }
+        System.out.println("\nVeiculo");
+        System.out.print("Modelo: ");
+        String modelo = scanner.nextLine();
+        System.out.print("Placa: ");
+        String placa = scanner.nextLine();
+        System.out.print("Chassi: ");
+        String chassi = scanner.nextLine();
+        System.out.print("Ano de Fabricação: ");
+        int anoFabricacao = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Cor: ");
+        String cor = scanner.nextLine();
 
-    public void setVeiculo(String modelo, String placa, String chassi, String cor, int anoFabricacao){
-
-        if(anoFabricacao < Veiculo.LIMITE_ANO_FABRICACAO)
-            throw new IllegalArgumentException("Ano de fabricação não aceito.");
-        veiculo.setVeiculo(modelo, placa, chassi, cor, anoFabricacao);
+        boolean alteracaoValida = false;
+        while(!alteracaoValida){
+            try{
+                veiculo.setVeiculo(modelo, placa, chassi, cor, anoFabricacao);
+                alteracaoValida = true;
+            }
+            catch(IllegalArgumentException e){
+                System.out.println("Erro ao alterar veiculo.");
+                System.out.println(e.getMessage());
+                System.out.println("Digite novamente o ano de fabricação: ");
+                anoFabricacao = scanner.nextInt();
+                scanner.nextLine();
+            }
+        }   
     }
 
 }
